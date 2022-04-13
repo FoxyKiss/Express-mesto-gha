849fcec16392c5dd216551c686e505dba61e5145
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-04-13 19:28:51 +0600

    feat: Создать схемы и модели card

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
-
-mongoose.connect('mongodb://localhost:27017/mestodb');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
 });
---
+++ models/card.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+
+  link: {
+    type: String,
+    required: true,
+  },
+
+  owner: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+
+  likes: [{
+    type: mongoose.Types.ObjectId,
+    required: true,
+    default: [],
+  }],
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('user', cardSchema);
