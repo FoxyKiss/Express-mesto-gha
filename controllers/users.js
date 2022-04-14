a151ade485037f7a9add21db9820caef6d3e0b5d
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-04-14 11:39:26 +0600

    refactor: Перенести функции  users в контроллеры

---
+++ controllers/users.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+
+function findAllUsers(req, res) {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+function findUserById(req, res) {
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+function createUser(req, res) {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+module.exports = { findAllUsers, findUserById, createUser };
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,10 @@
 const router = require('express').Router();
-const User = require('../models/user');
+const { findAllUsers, findUserById, createUser } = require('../controllers/users');
 
-router.get('/', (req, res) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-});
+router.get('/', findAllUsers);
 
-router.get('/:id', (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-});
+router.get('/:id', findUserById);
 
-router.post('/', (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
-});
+router.post('/', createUser);
 
 module.exports = router;
