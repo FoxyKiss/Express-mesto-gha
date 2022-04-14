949084a1c8fe75e8fd4d11ee05aef121fc388ae4
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-04-14 15:17:45 +0600

    refactor: рефакторинг названий выходных данных

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,7 +10,7 @@ function createCard(req, res) {
   const { name, link } = req.body;
   const owner = req.user._id
   Card.create({ name, link, owner })
-    .then((cards) => res.send({ data: cards }))
+    .then((card) => res.send({ data: card }))
     .catch((err) => {
       if(err.name === 'ValidationError') {
         res.status(400).send({message: 'Введены некорретные данные'})
@@ -22,8 +22,8 @@ function createCard(req, res) {
 }
 
 function deleteCard(req, res) {
-  Card.findByIdAndRemove(req.params.id)
-    .then((cards) => res.send({ data: cards }))
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send({ data: card }))
     .catch((err) => {
       if(err.name === 'CastError') {
         res.status(400).send({message: 'Карточка не найдена'})
