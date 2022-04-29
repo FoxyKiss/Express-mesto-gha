57e8a07e0cbe1fabec4519cd8a81f780ac461462
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-04-29 12:22:03 +0600

    "feat: Сделать так чтоб API не возвращал Хэш пароля"

--- models/user.js
+++ models/user.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
